927d5d15da1764339a315e6a8d6232a6ddfbcad9
kb <kb108904@gmail.com>
2024-01-07 01:28:56 -0500

    003 Refactor the Quiz component to use useReduce
       Remove the useState
       Add useReduce to manage state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,20 +1,28 @@
 import Question from "./Question"
-import { useState } from "react"
+import { useReducer } from "react"
+
+const initialState = {
+    currentQuestionIndex: 0,
+    question: [],
+};
+
+const reducer = (state, action) => {
+    if (action.type === "NEXT_QUESTION") { 
+        return { ...state, currentQuestionIndex: state.currentQuestionIndex+1};
+    }
+    return state;
+}
 
 const Quiz = () => {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-
-    const testClick = () => {
-        console.log("Test Click")
-        setCurrentQuestionIndex(currentQuestionIndex+1)
-    }
+    const [state, dispatch] = useReducer(reducer, initialState);
+    console.log("render", state);
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8</div>
                 <Question />
-                <div className="next-button" onClick={testClick}>
-                    Next question {currentQuestionIndex}
+                <div className="next-button" onClick={() => dispatch({type: 'NEXT_QUESTION'})}>
+                    Next question
                 </div>
             </div>
         </div>
