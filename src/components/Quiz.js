a51ccb221a3dd624fc38e051098a0f3a18aa1e2b
kb <kb108904@gmail.com>
2024-01-07 01:08:24 -0500

    002 Add state to the quiz component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,21 @@
 import Question from "./Question"
+import { useState } from "react"
 
 const Quiz = () => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+
+    const testClick = () => {
+        console.log("Test Click")
+        setCurrentQuestionIndex(currentQuestionIndex+1)
+    }
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8</div>
-                <Question/>
-                <div className="next-button">Next question</div>
+                <Question />
+                <div className="next-button" onClick={testClick}>
+                    Next question {currentQuestionIndex}
+                </div>
             </div>
         </div>
     )
