116471c55b3371164dac996bdea14f9edf9cee2f
kb <kb108904@gmail.com>
2024-01-07 00:51:51 -0500

    001 create components    Create a Answer component    Create a Question component    Create a Quiz component    Add the css and data file

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+      question: "Which Golden Age superhero has the alias 'The Scarlet Speedster'?",
+      incorrectAnswers: ["Green Lantern", "Hawkman", "Black Canary"],
+      correctAnswer: "The Flash",
+    },
+    {
+      question: "What is the civilian name of the hero who became the Green Lantern in the Golden Age?",
+      incorrectAnswers: ["Guy Gardner", "Hal Jordan", "John Stewart"],
+      correctAnswer: "Alan Scott",
+    },
+    {
+      question: "Which Golden Age hero has mystical powers that grants him super strength and invulnerability?",
+      incorrectAnswers: ["The Atom", "Wildcat", "Green Arrow"],
+      correctAnswer: "Shazam",
+    },
+    {
+      question: "What is the real name of the Golden Age superhero who is a skilled archer and wears a green feathered cap?",
+      incorrectAnswers: ["Hawkgirl", "Speedy", "Hawkman"],
+      correctAnswer: "Green Arrow",
+    },
+    {
+      question: "Which Golden Age superhero is a founding member of the Justice Society of America and wields a mace?",
+      incorrectAnswers: ["The Flash", "Green Lantern", "Sandman"],
+      correctAnswer: "Hawkman",
+    },
+    {
+      question: "What is the secret identity of the hero who uses a magical lasso that compels truthfulness?",
+      incorrectAnswers: ["Dinah Drake", "Shiera Sanders Hall", "Terry Sloane"],
+      correctAnswer: "Diana Prince",
+    },
+    {
+      question: "Which Golden Age superhero has the ability to shrink to atomic size?",
+      incorrectAnswers: ["Green Lantern", "Hawkman", "The Flash"],
+      correctAnswer: "The Atom",
+    },
+    {
+      question: "What is the name of the hero who fights crime with a gas mask and a trench coat in the Golden Age?",
+      incorrectAnswers: ["Plastic Man", "The Spirit", "The Question"],
+      correctAnswer: "Sandman",
+    },
+  ];
+  
+  export default data;
+  
